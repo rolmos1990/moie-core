159511df9e0793964e872611ade8d7a6337d4e43
Ramon Olmos <ramon.olmos90@gmail.com>
2021-04-05 11:03:09 -0500

    default server port and host

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+const PORT = process.env.PORT || 18210;
+const HOST = process.env.HOST || "http://54.226.170.69";
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at ${HOST}:${PORT}`);
