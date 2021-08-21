89057cd2df4f6ede15945f594777114912060af3
Ramon Olmos <ramon.olmos90@gmail.com>
2021-08-20 22:01:14 -0500

    changes - office and others

--- src/common/controllers/conditional.query.ts
+++ src/common/controllers/conditional.query.ts
@@ -39,13 +39,22 @@ export class ConditionalQuery {
                     field = query.split("$eq");
                     conditions.add(field[0], Operator.EQUAL, field[1]);
                 } else if (query.includes("$nnull")) {
-                    //compare is not null
                     field = query.split("$nnull");
-                    conditions.add(field[0], Operator.IS_NOT_NULL, null);
+                    if(field[0].includes(".")){
+                        const fieldSubField = field[0].split(".");
+                        conditions.addSub(field[0] + " IS NOT NULL ", {});
+                    } else {
+                        //compare is not null
+                        conditions.add(field[0], Operator.IS_NOT_NULL, null);
+                    }
                 } else if (query.includes("$null")) {
                     //compare is null
                     field = query.split("$null");
-                    conditions.add(field[0], Operator.IS_NULL, null);
+                    if(field[0].includes(".")){
+                        conditions.addSub(field[0] + " IS NULL ", {});
+                    } else {
+                        conditions.add(field[0], Operator.IS_NULL, null);
+                    }
                 } else if (query.includes("$true")) {
                     //compare is true
                     field = query.split("$true");
