009a74c7c21c5aaa3d3eb2cf26c8c7f7060b6ca6
Ramon Olmos <ramon.olmos90@gmail.com>
2021-08-29 11:16:33 -0500

    agregar prefijo a factura

--- src/controllers/parsers/bill.ts
+++ src/controllers/parsers/bill.ts
@@ -6,7 +6,7 @@ export const BillListDTO = (bill: Bill) => ({
     id: bill.id,
     createdAt: bill.createdAt,
     tax: bill.tax,
-    legalNumber: bill.legalNumber,
+    legalNumber: bill.billConfig && bill.billConfig.prefix ? bill.billConfig.prefix : ""  + "" + bill.legalNumber,
     status: bill.status,
     order: OrderListDTO(bill.order)
 });
