1715b60ed6fc31cea2dab4cc3d4e38fccbee8fdd
ramon <ramon.olmos90@gmail.com>
2021-11-28 11:26:11 -0500

    add payment controllar apply payment

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,14 +1,21 @@
-import {route} from 'awilix-express';
+import {POST, route} from 'awilix-express';
 import {PaymentService} from "../services/payment.service";
 import {BaseController} from "../common/controllers/base.controller";
 import {Payment} from "../models/Payment";
 import {EntityTarget} from "typeorm";
 import {PaymentCreateDTO} from "./parsers/payment";
+import {Request, Response} from "express";
+import {IProductSize} from "../common/interfaces/IProductSize";
+import {Product} from "../models/Product";
+import {ApplicationException, InvalidArgumentException} from "../common/exceptions";
+import {OrderService} from "../services/order.service";
+import {OrderStatus} from "../common/enum/orderStatus";
 
 @route('/payment')
 export class PaymentController extends BaseController<Payment> {
     constructor(
         private readonly paymentService: PaymentService,
+        private readonly orderService: OrderService
     ){
         super(paymentService);
     };
@@ -31,6 +38,36 @@ export class PaymentController extends BaseController<Payment> {
 
     public getEntityTarget(): EntityTarget<any> {
         return Payment;
+    }
+
+    @route('/applyPayment/:id')
+    @POST()
+    protected async applyPayment(req: Request, res: Response){
+        try {
+            const id = req.params.id;
+            const {orderId} = req.body || [];
+            if (orderId && id) {
+
+                const order = await this.orderService.find(orderId);
+                const paymentId = await this.paymentService.find(parseInt(id));
+
+                order.status = OrderStatus.RECONCILED;
+                order.payment = paymentId;
+                await this.orderService.update(order);
+
+                return res.json({status: 200 } );
+            } else {
+                throw new InvalidArgumentException();
+            }
+        }catch(e){
+            if (e.name === InvalidArgumentException.name || e.name === "EntityNotFound") {
+                this.handleException(new InvalidArgumentException("Orden no ha sido encontrada"), res);
+            }
+            else{
+                this.handleException(new ApplicationException(), res);
+
+            }
+        }
     }
 
     protected beforeCreate(item: Payment){}
