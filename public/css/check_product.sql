f9d5f82e92f0fc44fdf6ba0540bd6d8b22d3cf10
Ramon Olmos <ramon.olmos90@gmail.com>
2022-06-08 12:03:09 -0500

    add some changes for docker deployment

--- public/css/check_product.sql
+++
@@ -1,3 +0,0 @@
-select producto.*,categoria.*,if(producto.descuento_especial > 0,CEIL(producto.precio*((100-producto.descuento_especial)/100)),CEIL(producto.precio*((100-categoria.descuento)/100))) as precio_descuento,CEIL(producto.precio*((100-categoria.descuento_mayor_1)/100)) as precio_descuento_mayor_1,producto.descuento_especial "
-                    . "from producto inner join categoria on producto.id_categoria=categoria.id "
-                    . "where id_categoria=$id_categoria and existencia>0 order by producto.orden asc
