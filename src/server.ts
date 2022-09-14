b6923d14e20c7439276fd7c61eae56760273fad0
Ramon Olmos <ramon.olmos90@gmail.com>
2022-09-14 08:51:59 -0500

    change on server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { app } from './app';
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8086;
 const HOST = process.env.HOST;
+
+console.log('PORT TO RUN: ', PORT);
+console.log('HOST TO RUN: ', HOST);
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at ${HOST}:${PORT}`);
