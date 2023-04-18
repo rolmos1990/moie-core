864210b20947ea1d5f1cc4b36c0b432c15be9c61
Ramon Olmos <ramon.olmos90@gmail.com>
2023-04-18 02:24:52 -0500

    change on core for build catalog images 3

--- extension/pngconverter/util.js
+++ extension/pngconverter/util.js
@@ -5,10 +5,12 @@ class Converter {
         try {
             const browser = await puppeteer.launch({
                 executablePath: '/usr/bin/google-chrome',
-                args: ['--no-sandbox']});
+                dumpio: true,
+                ignoreHTTPSErrors: true,
+                args: ['--no-sandbox', '--disable-web-security', "--disable-setuid-sandbox"]});
             const page = await browser.newPage();
 
-            await page.goto('https://developer.chrome.com/');
+            await page.goto('http://moie.lucymodas.com/');
 
             // Configuración de la página
             const width = 788; // Ancho de la imagen en px
@@ -18,13 +20,15 @@ class Converter {
             await page.setViewport({width, height, deviceScaleFactor});
 
             // Cargar el HTML
-            await page.setContent(_html);
+            await page.setContent(_html, { waitUntil: 'networkidle2' });
             const screenshotBuffer = await page.screenshot({type: format});
+
+
             await browser.close();
             // Devolver la imagen como buffer
             return screenshotBuffer;
         } catch (e) {
-            console.log('error conviertiendo la imagen...');
+            console.log('error conviertiendo la imagen...', e.message);
             return false;
         }
     }
